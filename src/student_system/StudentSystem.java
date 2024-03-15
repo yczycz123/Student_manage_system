636d7130bb92f7b41f38c608be603fb19500b0a6
ycz <ycz_20011201@163.com>
2024-03-15 17:16:43 +0800

    2024/3/15

--- src/student_system/StudentSystem.java
+++ src/student_system/StudentSystem.java
@@ -19,8 +19,8 @@ public class StudentSystem {
             String choose=sc.next();
             switch (choose){
                 case "1": addStudent(list);break;
-                case "2": delectStudent();break;
-                case "3": changeStudent();break;
+                case "2": delectStudent(list);break;
+                case "3": changeStudent(list);break;
                 case "4": selectStudent(list);break;
                 case "5": {
                     System.out.println("退出");
@@ -49,7 +49,7 @@ public class StudentSystem {
 
             //判断id是否唯一
             boolean id_flag= id_contions(list,id);
-            if(id_flag){
+            if(!id_flag){
                 Student stu=new Student(id,name,age,address);
                 list.add(stu);
                 System.out.println("学生信息添加完毕");
@@ -61,13 +61,47 @@ public class StudentSystem {
     }
 
     //删除学生
-    public static void delectStudent(){
-        System.out.println("删除学生");
+    public static void delectStudent(ArrayList<Student>list){
+        System.out.println("请输入要删除学生的id");
+        Scanner sc=new Scanner(System.in);
+        String id=sc.next();
+       boolean flag= id_contions(list,id);
+       if(flag){
+           int index=getid_index(list,id);
+           list.remove(index);
+           System.out.println("id为"+id+"的学生信息删除成功");
+       }else{
+           System.out.println("id不存在");
+
+       }
     }
 
     //修改学生
-    public static void changeStudent(){
-        System.out.println("修改学生");
+    public static void changeStudent(ArrayList<Student>list){
+        System.out.println("请输入要修改的学生的id");
+        Scanner sc=new Scanner(System.in);
+        String id=sc.next();
+        boolean flag= id_contions(list,id);
+        if(flag){
+            int index=getid_index(list,id);
+            System.out.println("请输入修改后的学生id");
+            String new_id=sc.next();
+            System.out.println("请输入修改后的学生姓名");
+            String name=sc.next();
+            System.out.println("请输入修改后的学生年龄");
+            int age=sc.nextInt();
+            System.out.println("请输入修改后的学生家庭住址");
+            String address=sc.next();
+
+            Student s=list.get(index);
+            s.setId(new_id);
+            s.setName(name);
+            s.setAge(age);
+            s.setAddress(address);
+            System.out.println("学生信息修改成功");
+        }else{
+            System.out.println("id不存在");
+        }
     }
 
     //查询学生
@@ -84,14 +118,28 @@ public class StudentSystem {
         }
     }
 
+    //判断id是否唯一
     public static boolean id_contions(ArrayList<Student>list,String id){
         for (int i = 0; i < list.size(); i++) {
             Student stu=list.get(i);
             String stu_id=stu.getId();
             if(stu_id.equals(id)){
-                return  false;
+                return  true;
             }
         }
-        return true;
+        return false;
+    }
+
+
+    //返回得到的id索引值
+    public static int getid_index(ArrayList<Student>list,String id){
+        for (int i = 0; i < list.size(); i++) {
+            Student stu=list.get(i);
+            String stu_id=stu.getId();
+            if(stu_id.equals(id)){
+                return  i;
+            }
+        }
+        return -1;
     }
 }
